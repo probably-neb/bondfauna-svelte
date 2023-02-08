e0b82f137064d124c36ac69a862e3104b4061ce0
Ben Kunkle <ben.kunkle@gmail.com>
2023-02-08 11:04:11 -0800

    version control

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm'
+import { ViteRsw } from 'vite-plugin-rsw'
 import topLevelAwait from 'vite-plugin-top-level-await'
 
 const config: UserConfig = defineConfig({
-	plugins: [sveltekit(),wasm(),topLevelAwait()],
+	plugins: [sveltekit(),wasm(),topLevelAwait(),ViteRsw()],
     server: {
         fs: {
             allow: [
-                './roget/pkg/'
+                './roget/pkg/',
+                './wasm-wordle/pkg/'
             ]
         }
     }
